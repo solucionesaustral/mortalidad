da9c02e007add7d3b453eec36ad91601a89e0235
solucionesaustral <136752716+solucionesaustral@users.noreply.github.com>
2023-06-20 18:03:40 -0400

    Add files via upload

---
+++ llaguepe-mortalidad.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 29 09:09:59 2023
+
+@author: mauricio
+"""
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.express as px
+import pandas as pd
+
+
+df = pd.read_csv('c:\Python2\Llaguepe2.csv', delimiter = ';')
+fr = pd.DataFrame(df)
+ 
+  
+app=dash.Dash()
+
+
+fig = px.line(fr, x='fecha', y= "mortalidad", title= "Gráfico de Mortalidad", template = "plotly_dark")
+
+app.layout=html.Div([
+     
+            html.H1(children='Mortalidades'),
+            html.Div(children='''
+                Centro de cultivo
+            '''),
+            dcc.Graph(id='ejemplo', 
+                      figure=fig)
+ 
+]) 
+                     
+
+                     
+if __name__ == "__main__":
+    app.run_server(debug=False)
+
