8092787481af7bf920a4ce3448912271074a64c1
solucionesaustral <136752716+solucionesaustral@users.noreply.github.com>
2023-06-20 19:27:22 -0400

    Update llaguepe_mortalidad.py

--- llaguepe_mortalidad.py
+++ llaguepe_mortalidad.py
@@ -9,29 +9,22 @@ from dash import Dash, dcc, html
 import plotly.express as px
 import pandas as pd
 
-
 df = pd.read_csv('http://solucionesaustral.cl/Llaguepe2.csv', delimiter = ';')
-fr = pd.DataFrame(df)
- 
   
 app = Dash(__name__)
 server = app.server
 
-fig = px.line(fr, x='fecha', y= "mortalidad", title= "Gráfico de Mortalidad", template = "plotly_dark")
+fig = px.line(df, x='fecha', y= "mortalidad", title= "Gráfico de Mortalidad", template = "plotly_dark")
 
-app.layout=html.Div([
-     
+app.layout=html.Div([     
             html.H1(children='Mortalidades'),
             html.Div(children='''
                 Centro de cultivo
             '''),
             dcc.Graph(id='ejemplo', 
                       figure=fig)
- 
-]) 
-                     
-
-                     
+])                   
+                    
 if __name__ == "__main__":
     app.run_server(debug=False)
 
